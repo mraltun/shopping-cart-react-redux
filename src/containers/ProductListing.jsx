348666f3b2fec115ebde25013094ea7b85f7d718
Murat Altun <mraltun@gmail.com>
2022-08-27 14:59:39 +0300

    Implemented set_products in reducer

--- src/containers/ProductListing.jsx
+++ src/containers/ProductListing.jsx
@@ -1,9 +1,23 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { setProducts } from "../redux/actions/productActions";
 import ProductComponent from "./ProductComponent";
 
 const ProductListing = () => {
   const products = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const fetchProducts = async () => {
+    const response = await axios("https://fakestoreapi.com/products").catch(
+      (err) => console.log("Error", err)
+    );
+    dispatch(setProducts(response.data));
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   return (
     <div className='ui grid container'>
