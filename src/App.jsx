54dfb01ede66c3d10563750ae988d815f8d7937f
Murat Altun <mraltun@gmail.com>
2022-08-27 13:51:58 +0300

    Added basic Header and routes in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import ProductDetails from "./containers/ProductDetails";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<ProductListing />} />
+          <Route path='product/:productId' element={<ProductDetails />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
