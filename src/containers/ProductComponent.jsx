f468dca87bf32010196a199523c7889043e638bd
Murat Altun <mraltun@gmail.com>
2022-08-27 14:37:49 +0300

    Using useSelector inside ProductListing and ProductComponent

--- src/containers/ProductComponent.jsx
+++ src/containers/ProductComponent.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const ProductComponent = () => {
-  return <div>ProductComponent</div>;
+  const products = useSelector((state) => state.allProducts.products);
+  const { id, title } = products;
+
+  return (
+    <div className='four column wide'>
+      <div className='ui link cards'>
+        <div className='card'>
+          <div className='image'></div>
+          <div className='content'>
+            <div className='header'>{title}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductComponent;
--- src/containers/ProductListing.jsx
+++ src/containers/ProductListing.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import ProductComponent from "./ProductComponent";
 
 const ProductListing = () => {
-  return <div>ProductListing</div>;
+  const products = useSelector((state) => state);
+
+  return (
+    <div className='ui grid container'>
+      <ProductComponent />
+    </div>
+  );
 };
 
 export default ProductListing;
