046810f9025ca5a2f3a0facffebb54350ab697bf
Murat Altun <mraltun@gmail.com>
2022-08-28 12:12:27 +0300

    Added App.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./containers/Header";
 import ProductListing from "./containers/ProductListing";
 import ProductDetails from "./containers/ProductDetails";
+import "./App.css";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<ProductListing />} />
           <Route path='product/:productId' element={<ProductDetails />} />
+          <Route path='*' element={<h1>404 - Not Found!</h1>} />
         </Routes>
       </BrowserRouter>
     </div>
