2b16dea3fe9b82b0ddb863a00a2c4bad2d9c1094
Murat Altun <mraltun@gmail.com>
2022-08-29 15:02:39 +0300

    Removed axios and side effects from ProductListing

--- src/containers/ProductListing.jsx
+++ src/containers/ProductListing.jsx
@@ -1,21 +1,12 @@
 import React, { useEffect } from "react";
-import axios from "axios";
 import { useSelector, useDispatch } from "react-redux";
-import { setProducts, fetchProducts } from "../redux/actions/productActions";
+import { fetchProducts } from "../redux/actions/productActions";
 import ProductComponent from "./ProductComponent";
 
 const ProductListing = () => {
   // Get the state from redux store
   const products = useSelector((state) => state);
   const dispatch = useDispatch();
-
-  // const fetchProducts = async () => {
-  //   const response = await axios("https://fakestoreapi.com/products").catch(
-  //     (err) => console.log("Error", err)
-  //   );
-  //   // Use dispatch to set state with action
-  //   dispatch(setProducts(response.data));
-  // };
 
   useEffect(() => {
     dispatch(fetchProducts());
